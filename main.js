4742aa914df45e435b7c1e6816a26c3a7a0c1d79
Houston-Smith <houstonbsmith@gmail.com>
2022-02-17 12:57:02 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+const fillGasTank = (gallons) => {
+  if (gallons > 15) {
+  console.log('You cannot add this much gas to the tank.') 
+  }
+  else {
+    console.log(`I filled the tank with ${gallons} gallons of gas.`)
+  }
+}
+
+fillGasTank (9);
+fillGasTank (17);
